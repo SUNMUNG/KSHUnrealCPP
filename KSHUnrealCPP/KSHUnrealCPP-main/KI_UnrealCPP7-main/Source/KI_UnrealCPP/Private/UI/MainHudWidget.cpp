6fd1031c9e6db925d99863aaa2afc91550a1d32d
KimSungHyun <144310761+SUNMUNG@users.noreply.github.com>
2025-12-01 10:58:58 +0900

    1201-인벤토리 위젯 추가 (1)

--- KSHUnrealCPP/KSHUnrealCPP-main/KI_UnrealCPP7-main/Source/KI_UnrealCPP/Private/UI/MainHudWidget.cpp
+++ KSHUnrealCPP/KSHUnrealCPP-main/KI_UnrealCPP7-main/Source/KI_UnrealCPP/Private/UI/MainHudWidget.cpp
@@ -4,18 +4,46 @@
 #include "UI/MainHudWidget.h"
 #include "Player/ActionCharacter.h"
 #include "Player/ResourceComponent.h"
+#include "UI/Inventory/InventoryWidget.h"
 #include "UI/ResourceBarWidget.h"
+
 
 void UMainHudWidget::NativeConstruct()
 {
 	AActionCharacter* player = Cast<AActionCharacter>(GetOwningPlayerPawn());
-	if (player && player->GetResourceComponent())
-	{
-		UResourceComponent* resource = player->GetResourceComponent();
-		resource->OnHealthChanged.AddUObject(HealthBar.Get(), &UResourceBarWidget::RefreshWidget);
-		resource->OnStaminaChanged.AddDynamic(StaminaBar.Get(), &UResourceBarWidget::RefreshWidget);		
+	if (player) {
+		if (player->GetResourceComponent())
+		{
+			UResourceComponent* resource = player->GetResourceComponent();
+			resource->OnHealthChanged.AddUObject(HealthBar.Get(), &UResourceBarWidget::RefreshWidget);
+			resource->OnStaminaChanged.AddDynamic(StaminaBar.Get(), &UResourceBarWidget::RefreshWidget);
 
-		HealthBar->RefreshWidget(resource->GetCurrentHealth(), resource->GetMaxHealth());
-		StaminaBar->RefreshWidget(resource->GetCurrentStamina(), resource->GetMaxStamina());
+			HealthBar->RefreshWidget(resource->GetCurrentHealth(), resource->GetMaxHealth());
+			StaminaBar->RefreshWidget(resource->GetCurrentStamina(), resource->GetMaxStamina());
+		}
+
+		if (UInventoryComponent* inventoryComponent = player->GetInventoryComponent()) {
+			Inventory->OnInventoryCloseRequested.AddDynamic(this, &UMainHudWidget::CloseInventory);
+		}
+	
+	}
+	
+	
+	
+	
+}
+
+void UMainHudWidget::OpenInventory()
+{
+	if (Inventory) {
+		Inventory->SetVisibility(ESlateVisibility::Visible);
+	}
+	
+}
+
+void UMainHudWidget::CloseInventory()
+{
+	if (Inventory) {
+		Inventory->SetVisibility(ESlateVisibility::Hidden);
 	}
 }
---
+++ KSHUnrealCPP/KSHUnrealCPP-main/KI_UnrealCPP7-main/Source/KI_UnrealCPP/Public/UI/Inventory/InventoryWidget.h
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "InventoryWidget.generated.h"
+
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnInventoryCloseRequested);
+
+/**
+ * 
+ */
+
+UCLASS()
+class KI_UNREALCPP_API UInventoryWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+public:
+	virtual void NativeConstruct() override;
+private:
+	UFUNCTION()
+	void OnCloseClicked();
+public:
+	UPROPERTY(BlueprintAssignable, Category = "UI|Inventory")
+	FOnInventoryCloseRequested OnInventoryCloseRequested;
+protected:
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<class UButton> CloseButton = nullptr;
+private:
+	
+
+
+};
