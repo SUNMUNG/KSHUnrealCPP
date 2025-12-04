e7483c32e7fafd255bc42a7539d3db6e70e05dfb
KimSungHyun <144310761+SUNMUNG@users.noreply.github.com>
2025-12-04 17:56:22 +0900

    1204-아이템 상점 구현(2)

--- KSHUnrealCPP-main/Source/KI_UnrealCPP/Private/UI/Shop/ItemBuyWidget.cpp
+++ KSHUnrealCPP-main/Source/KI_UnrealCPP/Private/UI/Shop/ItemBuyWidget.cpp
@@ -2,4 +2,61 @@
 
 
 #include "UI/Shop/ItemBuyWidget.h"
+#include "Components/Button.h"
+#include "Components/Image.h"
+#include "Components/TextBlock.h"
+#include "Components/EditableTextBox.h"
+#include "Components/Overlay.h"
+#include "Data/ItemDataAsset.h"
+#include "Player/InventoryOwner.h"
 
+
+void UItemBuyWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+
+	if (ItemCount) {
+		ItemCount->OnTextChanged.AddDynamic(this, &UItemBuyWidget::OnItemCountTextChanged);
+		ItemCount->OnTextCommitted.AddDynamic(this, &UItemBuyWidget::OnItemCountTextCommitted);
+
+		ItemCount->SetHintText(FText::AsNumber(MinimumItemCount));
+	}
+	if (ItemStackCount) {
+		MaxStackCount = FCString::Atoi(*ItemStackCount->GetText().ToString());
+	}
+}
+
+void UItemBuyWidget::OnItemCountTextChanged(const FText& Text)
+{
+	FString number = Text.ToString();
+
+	if (number.IsNumeric()) {
+		int32 count = FCString::Atoi(*number);
+		ItemCount->SetText(FText::AsNumber(count));
+	}
+
+}
+
+void UItemBuyWidget::OnItemCountTextCommitted(const FText& Text, ETextCommit::Type CommitMethod)
+{
+	FString number = Text.ToString();
+
+
+	if (number.IsNumeric()) {
+		int32 count = FCString::Atoi(*number);
+
+		UE_LOG(LogTemp, Log, TEXT("%d %d"), count, MaxStackCount);
+		if (MaxStackCount > count) {
+			ItemCount->SetText(FText::AsNumber(count));
+		}
+		else {
+			ItemCount->SetText(FText::AsNumber(MaxStackCount));
+		}
+
+	}
+	else {
+		ItemCount->SetText(FText::AsNumber(MinimumItemCount));
+	}
+
+	
+}
--- KSHUnrealCPP-main/Source/KI_UnrealCPP/Public/UI/Shop/ItemBuyWidget.h
+++ KSHUnrealCPP-main/Source/KI_UnrealCPP/Public/UI/Shop/ItemBuyWidget.h
@@ -6,6 +6,11 @@
 #include "Blueprint/UserWidget.h"
 #include "ItemBuyWidget.generated.h"
 
+class UImage;
+class UTextBlock;
+class UEditableTextBox;
+class UButton;
+class UOverlay;
 /**
  * 
  */
@@ -13,6 +18,41 @@ UCLASS()
 class KI_UNREALCPP_API UItemBuyWidget : public UUserWidget
 {
 	GENERATED_BODY()
+protected:
+	virtual void NativeConstruct() override;
 	
+private:
+	UFUNCTION()
+	void OnItemCountTextChanged(const FText& Text);
 
+	UFUNCTION()
+	void OnItemCountTextCommitted(const FText& Text, ETextCommit::Type CommitMethod);
+protected:
+	UPROPERTY(BlueprintReadOnly, Category = "Shop|ItemBuy", meta = (BindWidget))
+	TObjectPtr<UImage> ItemIcon = nullptr;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Shop|ItemBuy", meta = (BindWidget))
+	TObjectPtr<UTextBlock> ItemName = nullptr;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Shop|ItemBuy", meta = (BindWidget))
+	TObjectPtr<UTextBlock> ItemPrice = nullptr;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Shop|ItemBuy", meta = (BindWidget))
+	TObjectPtr<UTextBlock> ItemDescription = nullptr;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Shop|ItemBuy", meta = (BindWidget))
+	TObjectPtr<UTextBlock> ItemStackCount = nullptr;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Shop|ItemBuy", meta = (BindWidget))
+	TObjectPtr<UOverlay> Soldout = nullptr;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Shop|ItemBuy", meta = (BindWidget))
+	TObjectPtr<UEditableTextBox> ItemCount = nullptr;
+	
+	UPROPERTY(BlueprintReadOnly, Category = "Shop|ItemBuy", meta = (BindWidget))
+	TObjectPtr<UButton> ItemBuy = nullptr;
+private:
+	static const int32 MinimumItemCount = 1;
+
+	int32 MaxStackCount = -1;
 };
